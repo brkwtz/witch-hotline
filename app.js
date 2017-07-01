9e8881ac2b37a894c4c7bed53862eb9a9c1af492
Rachel Berkowitz <brkwtzr@gmail.com>
2017-07-01 13:16:44 -0400

    delete record after text sent

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const secrets = require('./secrets')
+const firebase = require('./fire')
+const bluebird = require('bluebird')
 
 // Twilio Credentials
 const accountSid = secrets.TWILIOCLIENTSID
@@ -16,22 +18,31 @@ const client = require('twilio')(accountSid, authToken)
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '/public')))
 
 //listen
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on some port')
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+  console.log('listening on port', port)
+})
+
+//authenticate with firebase
+firebase.auth().onAuthStateChanged(user => user || firebase.auth().signInAnonymously())
+
+//listen for changes to sms
+firebase.database().ref('sms').on('child_added', function(snapshot) {
+  const targetPhone = snapshot.val().targetPhone
+  const messageBody = snapshot.val().messageBody
+
+  client.messages.create({
+    to: targetPhone,
+    from: '+18316618986',
+    body: messageBody
+  })
+  .then(() => {
+    firebase.database().ref('sms').remove()
+  })
 })
 
 app.get('/', (req, res, next) => {
   res.send("shaniqua don't live here no more")
-})
-
-app.post('/', (req, res, next) => {
-  client.messages.create({
-    to: '+1' + req.body.targetPhone,
-    from: '+18316618986',
-    body: req.body.messageBody
-  })
-  .then(message => res.send(message.sid))
 })
